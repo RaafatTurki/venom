e17df7ff02e9843e9d83858614e21fe4e7995af3
Raafat Turki <raafat.turki@pm.me>
2022-04-15 03:59:57 +0300

    removed gitignore

---
+++ lua/services/lang.lua
@@ -0,0 +1,135 @@
+--- defines language specific configurations
+-- @module lang
+local M = {}
+
+M.configure_server = U.Service():require(FT.LSP, 'setup'):new(function(name, is_auto_installed, opts)
+  local available_server_names = require 'nvim-lsp-installer.servers'.get_available_server_names()
+
+  if U.has_value(available_server_names, name) then
+    local server_config = U.LspServerConfig():auto_install(is_auto_installed):new(name, opts)
+    Lsp.add_server_config:invoke(server_config)
+    log("["..name.."] added lsp server configurations.", LL.DEBUG)
+  else
+    log("["..name.."] no such server available!", LL.WARN)
+  end
+end)
+
+M.configure_servers = U.Service():require(FT.LSP, 'setup'):new(function()
+
+  M.configure_server:invoke("sumneko_lua", true, {
+    settings = {
+      Lua = {
+        -- runtime = {
+        --  version = 'LuaJIT',
+        --  path = vim.split(package.path, ';'),
+        -- },
+        diagnostics = {
+          disable = { 'lowercase-global', 'trailing-space', 'unused-local' },
+          globals = { 'vim' },
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
+        },
+        telemetry = { enable = false },
+      }
+    }
+  })
+
+  M.configure_server:invoke("texlab", false, {
+    settings = {
+      texlab = {
+        build = {
+          -- forwardSearchAfter = true,
+          onSave = true,
+          executable = 'tectonic',
+          args = vim.split('%f --synctex', ' '),
+          -- "--synctex",
+          -- "--keep-logs",
+          -- "--keep-intermediates"
+          -- "--outdir out",
+          -- "--outfmt pdf", -- pdf, html, xdv, aux, fmt
+          -- },
+        },
+        forwardSearch = {
+          executable = "zathura",
+          args = {"--synctex-forward", "%l:1:%f", "%p"},
+        },
+      },
+      -- chktex = {
+      --  onEdit = true,
+      --  onOpenAndSave = true,
+      -- },
+    }
+  })
+
+  M.configure_server:invoke("svelte", false, {
+    settings = {
+      svelte = {
+        plugin = {
+          svelte = {
+            format = { enable = false },
+            compilerWarnings = {
+              ["css-unused-selector"] = 'ignore',
+              ["a11y-missing-attribute"] = 'ignore',
+              ["a11y-missing-content "] = 'ignore',
+              -- ["unused-export-let"] = 'ignore',
+            }
+          }
+        }
+      }
+    }
+  })
+
+  M.configure_server:invoke("rust_analyzer", false, {
+    settings = {
+      ["rust-analyzer"] = {
+        diagnostics = {
+          disabled = true
+          -- disabled = {
+          --  "unresolved-import",
+          -- }
+        }
+      }
+    }
+  })
+
+  M.configure_server:invoke("emmet_ls", false, {
+    filetypes = { 'html', 'css', 'svelte' },
+  })
+
+  M.configure_server:invoke("jsonls", false, {
+    settings = {
+      json = {
+        -- visit https://www.schemastore.org/json/ for more schemas
+        schemas = {
+          { fileMatch = { 'package.json' }, url = 'https://json.schemastore.org/package.json' },
+          { fileMatch = { 'tsconfig.json', 'tsconfig.*.json' }, url = 'http://json.schemastore.org/tsconfig' },
+          { fileMatch = { '.eslintrc.json', '.eslintrc' }, url = 'http://json.schemastore.org/eslintrc' },
+          { fileMatch = { '.prettierrc', '.prettierrc.json', 'prettier.config.json' }, url = 'http://json.schemastore.org/prettierrc' },
+          { fileMatch = { 'deno.json' }, url = 'https://raw.githubusercontent.com/denoland/deno/main/cli/schemas/config-file.v1.json' },
+          { fileMatch = { '.swcrc' }, url = 'https://json.schemastore.org/swcrc.json' },
+        },
+      },
+    }
+  })
+
+  -- M.configure_server:invoke("$1", $2, {
+  --  $3
+  -- })
+
+--- language servers options that are installed by third party means
+-- @field table ls opts
+-- M.SERVERS_THIRD_PARTY_OPTS = {
+--   gdscript = {
+--     cmd = {'godot-ls'},
+--     flags = {
+--       debounce_text_changes = 150,
+--     },
+--   },
+-- }
+end)
+
+return M
---
+++ lua/services/lsp.lua
@@ -0,0 +1,117 @@
+--- defines how language servers are installed and setup
+-- @module lsp
+
+-- a langaue servers must be setup in order to be used, there are two types of language servers:
+-- available servers      these are installed and setup through nvim-lsp-installer (which uses lspconfig internally)
+-- WIP: third party servers    these are installed by 3rd party means and setup through lspconfig
+
+local M = {}
+
+M.servers_configs = {}
+M.shared_server_config = {}
+
+M.add_server_config = U.Service():require(FT.LSP, 'setup'):new(function(server_config)
+  table.insert(M.servers_configs, server_config)
+end)
+
+M.install_auto_installable_servers = U.Service():require(FT.LSP, 'setup'):new(function()
+  for _, server_config in pairs(M.servers_configs) do
+    if (server_config.is_auto_installed) then
+
+      -- TODO: abstract into a M.get_server service and handle ok value with catch
+      local ok, ls = require 'nvim-lsp-installer.servers'.get_server(server_config.name)
+      if ok then
+        if not ls:is_installed() then
+          log("["..server_config.name.."] installing")
+          -- TODO: attach the logging to a post complete hook
+          ls:install()
+          -- log("["..server_config.name.."] installed")
+        end
+      else
+        log("["..server_config.name.."] no server available with such name", LL.ERROR)
+      end
+
+    end
+  end
+end)
+
+M.setup_servers = U.Service():require(FT.LSP, 'setup'):new(function()
+
+  for _, server_config in pairs(M.servers_configs) do
+    log("["..server_config.name.."] setting up lsp server.", LL.DEBUG)
+
+    -- TODO: use vim.tbl_extend and vim.tbl_deep_extend
+    server_config.opts.on_attach = M.shared_server_config.opts.on_attach
+    server_config.opts.capabilities = M.shared_server_config.opts.capabilities
+    server_config.opts.handlers = M.shared_server_config.opts.handlers
+
+    -- TODO: abstract into a M.get_server service and handle ok value with catch
+    local ok, ls = require 'nvim-lsp-installer.servers'.get_server(server_config.name)
+    if ok then
+      ls:setup(server_config.opts)
+      vim.cmd [[do User LspAttachBuffers]]
+    end
+
+  end
+end)
+
+M.setup = U.Service():provide(FT.LSP, 'setup'):require(FT.PLUGIN, 'nvim-lsp-installer'):require(FT.PLUGIN, 'nvim-lspconfig'):new(function()
+    local lspconfig = require 'lspconfig'
+    local lspinstaller = require 'nvim-lsp-installer'
+
+    lspinstaller.settings({
+      ui = {
+        icons = {
+          server_installed = " ",
+          server_pending = " ",
+          server_uninstalled = "  ",
+        },
+        keymaps = {
+          toggle_server_expand = "<Space>",
+          install_server = "<CR>",
+          update_server = "<CR>",
+          uninstall_server = "<BS>",
+        },
+      },
+      max_concurrent_installers = 3,
+    })
+
+
+    -- TODO: break into actions and features
+    M.shared_server_config = U.LspServerConfig():new("SHARED", {
+      -- document highlight on cursor hold if available
+      on_attach = function (client, bufnr)
+        if client.resolved_capabilities.document_highlight then
+          U.create_augroup([[
+            au CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+            au CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+          ]], 'hover_highlight')
+        end
+      end,
+
+      -- cmp autocompletion
+      capabilities = require 'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+
+      -- method handlers settings
+      handlers = {
+        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' }),
+        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' })
+      }
+    })
+
+
+    for type, icon in pairs(venom.icons.diagnostic_states.cozette) do
+      local hl = "DiagnosticSign" .. type
+      -- if (LSP_DIAG_ICONS == lsp_diag_icons.none) then icon = nil end
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+end)
+
+return M
+
+-- get_available_servers()
+-- get_installed_servers()
+-- get_uninstalled_servers()
+-- register({server})
+-- get_server({server_name})

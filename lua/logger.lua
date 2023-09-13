fc036d3bff500b9ec90fca71d44c6a3cf615f45e
Raafat Turki <raafat.turki@proton.me>
2023-09-14 02:51:43 +0300

    fix(logger): use vim.schedule instead of manually chaching logs, use vim.notify instead of nvim_echo

--- lua/logger.lua
+++ lua/logger.lua
@@ -1,26 +1,10 @@
 local M = {}
 
-local cached_logs = {}
-
 local last_log = {
   message = "",
   log_level = 0,
   count = 1,
 }
-
-local highlights = {
-  source = "Comment",
-  count = "Folded",
-  log_levels = {
-    [vim.log.levels.DEBUG] = 'Debug',
-    [vim.log.levels.ERROR] = 'DiagnosticFloatingError',
-    [vim.log.levels.INFO] = 'DiagnosticFloatingInfo',
-    [vim.log.levels.TRACE] = 'DiagnosticFloatingHint',
-    [vim.log.levels.WARN] = 'DiagnosticFloatingWarn',
-    [vim.log.levels.OFF] = 'Comment',
-  }
-}
-
 
 local get_caller_src = function(stack_level_offset)
   local dbg_info = debug.getinfo(4 + stack_level_offset, "Sl")
@@ -29,14 +13,16 @@ local get_caller_src = function(stack_level_offset)
 end
 
 local sanitize_value = function(val)
-  if type(val) ~= 'string' then
-    val = vim.inspect(val)
+  if type(val) == 'string' then
+    return val
+  else
+    return vim.inspect(val)
   end
-  return val
 end
 
-local process = function(val, opts, src)
+local process = function(val, opts)
   val = sanitize_value(val)
+  local src = get_caller_src(opts.stack_level_offset or 0)
 
   -- repeat log counting
   if last_log.message == val and opts.log_level == last_log.log_level then
@@ -49,32 +35,18 @@ local process = function(val, opts, src)
 
   local count = last_log.count > 1 and '×' .. last_log.count or '  '
 
-  vim.api.nvim_echo({
-    { count, highlights.count },
-    { ' ', '' },
-    { src, highlights.source },
-    { ' ', '' },
-    { val, highlights.log_levels[opts.log_level] },
-  }, true, {})
-end
-
-local preprocess = function(val, opts)
-  local src = get_caller_src(opts.stack_level_offset or 0)
-
-  if vim.v.vim_did_enter == 1 then
-    process(val, opts, src)
-  else
-    table.insert(cached_logs, { val = val, opts = opts, src = src })
-  end
+  vim.schedule(function()
+    vim.notify(val, opts.log_level, { title = count .. ' ' .. src })
+  end)
 end
 
 M.log = {
-  dbg   = function(val, opts) preprocess(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.DEBUG })) end,
-  err   = function(val, opts) preprocess(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.ERROR })) end,
-  info  = function(val, opts) preprocess(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.INFO })) end,
-  trace = function(val, opts) preprocess(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.TRACE })) end,
-  warn  = function(val, opts) preprocess(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.WARN })) end,
-  off   = function(val, opts) preprocess(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.OFF })) end,
+  dbg   = function(val, opts) process(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.DEBUG })) end,
+  err   = function(val, opts) process(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.ERROR })) end,
+  info  = function(val, opts) process(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.INFO })) end,
+  trace = function(val, opts) process(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.TRACE })) end,
+  warn  = function(val, opts) process(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.WARN })) end,
+  off   = function(val, opts) process(val, vim.tbl_deep_extend('force', opts or {}, { log_level = vim.log.levels.OFF })) end,
 }
 
 setmetatable(M.log, {
@@ -85,12 +57,5 @@ setmetatable(M.log, {
     self.info(val, opts)
   end
 })
-
--- process all cached_logs on enter
-events.enter:sub(function()
-  for i, log in ipairs(cached_logs) do
-    process(log.val, log.opts, log.src)
-  end
-end)
 
 return M

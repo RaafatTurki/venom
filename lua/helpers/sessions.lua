1545f0b0ea596a71c8f804449c15b3ae6ccaeba2
Raafat Turki <raafat.turki@proton.me>
2023-11-15 01:46:54 +0300

    fix: add session data types

--- lua/helpers/sessions.lua
+++ lua/helpers/sessions.lua
@@ -6,63 +6,55 @@ local M = {}
 M.local_session_file = vim.fn.getcwd() .. '/.venom.json'
 M.is_in_local_session = false
 
--- TODO: make session managment git branch aware
-
 vim.api.nvim_create_autocmd('VimLeavePre', {
   callback = function(ev)
     if M.is_in_local_session then M.save() end
   end
 })
 
--- TODO: save session on fs update event
--- events.fs_update:sub(function()
---   if M.is_in_local_session then
---     M.save()
---   end
--- end)
-
--- TODO: save session on buflist update
--- events.buflist_update:sub(function()
---   if M.is_in_local_session then
---     M.save()
---   end
--- end)
-
+---@class SessionData
+---@field buffers { current_file_index: number, file_paths: string[] }
 
 function M.save()
-  -- write session file
-  U.file_write(M.local_session_file, vim.fn.json_encode({
+  ---@type SessionData
+  local sdata = {
     buffers = buffers.aggregate(),
     -- dap = Dap.aggregate(),
-  }))
+  }
 
-  -- load if session isn't loaded
+  -- write session file
+  U.file_write(M.local_session_file, vim.fn.json_encode(sdata))
+
+  -- load if session isn't already loaded
   if not M.is_in_local_session then M.load() end
 end
 
 function M.load()
-  if U.is_file_exists(M.local_session_file) then
-    -- read session file
-    local json = U.file_read(M.local_session_file)
-    local decoded_data = vim.fn.json_decode(json)
-
-    -- populate modules with decoded data
-    if decoded_data then
-      buffers.populate(decoded_data.buffers)
-      -- Dap.populate(decoded_data.dap)
-
-      -- set local session bool
-      M.is_in_local_session = true
-    else
-      log.err("local session data is corrupted")
-    end
-  else
+  -- create a new session file if doesn't exist
+  if not U.is_file_exists(M.local_session_file) then
     vim.schedule(function()
       if U.confirm_yes_no('Local session file does not exist. Create one?') then
         M.save()
       end
     end)
+    return
   end
+
+  -- read session file
+  ---@type SessionData?
+  local sdata = vim.fn.json_decode(U.file_read(M.local_session_file))
+  if not sdata then
+    log.err("local session data is corrupted")
+    return
+  end
+
+  -- populate modules with decoded data
+  -- curr_branch = decoded_data.git_branches[U.get_curr_git_branch()]
+  buffers.populate(sdata.buffers)
+  -- Dap.populate(decoded_data.dap)
+
+  -- set local session bool
+  M.is_in_local_session = true
 end
 
 function M.delete()

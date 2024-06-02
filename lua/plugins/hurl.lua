31d36203b6181e7c110680e164d68c54dffedac7
Raafat Turki <raafat.turki@proton.me>
2024-06-02 12:49:32 +0300

    feat: add hurl.nvim

---
+++ lua/plugins/hurl.lua
@@ -0,0 +1,27 @@
+local plugins_info = require "helpers.plugins_info"
+
+local M = { plugins_info.hurl.url }
+
+M.dependencies = {
+  plugins_info.plenary.url,
+  plugins_info.nui.url,
+}
+
+M.ft = "hurl"
+
+M.config = function()
+  require "hurl".setup {
+    -- show_notification = true,
+    env_file = {
+      '.env',
+    },
+    mode = "popup",
+    popup_position = '0%',
+    popup_size = {
+      width = '100%',
+      height = '100%',
+    },
+  }
+end
+
+return M

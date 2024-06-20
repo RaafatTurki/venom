6a97a3e82b2a35915e8810c04be0c0f4e73a2e53
Raafat Turki <raafat.turki@proton.me>
2024-06-20 14:32:52 +0300

    feat: add mini.diff mappings, mini.git

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -88,6 +88,131 @@ M.config = function()
     keys.map("x", "<A-Up>",     function() mini_move.move_selection('up') end, "Visual shift lines up")
   end
 
+  local mini_trailspace = require 'mini.trailspace'
+  if mini_trailspace then
+    mini_trailspace.setup()
+    vim.api.nvim_create_user_command('Trim', mini_trailspace.trim, {})
+    vim.api.nvim_create_user_command('TrimLastLines', mini_trailspace.trim_last_lines, {})
+    vim.api.nvim_create_user_command('TrimAll', function()
+      mini_trailspace.trim()
+      mini_trailspace.trim_last_lines()
+    end, {})
+  end
+
+  local mini_extra = require "mini.extra"
+
+  local mini_pick = require 'mini.pick'
+  if mini_pick then
+    mini_pick.setup {
+      mappings = {
+        -- choose_marked = "<C-q>",
+      },
+      window = {
+        config = function()
+          height = math.floor(0.6 * vim.o.lines)
+          width = math.floor(0.6 * vim.o.columns)
+
+          return {
+            anchor = 'NW',
+            height = height,
+            width = width,
+            row = math.floor(0.5 * (vim.o.lines - height)),
+            col = math.floor(0.5 * (vim.o.columns - width)),
+          }
+        end
+      },
+    }
+
+    keys.map("n", "<leader><CR>", "<CMD>Pick resume<CR>", "Pick resume")
+    keys.map("n", "<leader>f",    "<CMD>Pick files<CR>", "Pick find files")
+    keys.map("n", "<leader>g",    "<CMD>Pick grep_live<CR>", "Pick grep string")
+    keys.map("n", "<leader>h",    "<CMD>Pick help<CR>", "Pick help pages")
+
+    -- if mini_extra then
+    -- keys.map("n", "<leader>b",    "<CMD>Pick buffer_lines<CR>", "Pick buffer lines")
+    -- keys.map("n", "<leader>b",    "<CMD>Pick diagnostic<CR>", "Pick lsp diagnostics")
+    -- keys.map("n", "<leader>b",    "<CMD>Pick explorer<CR>", "Pick file tree explorer")
+    -- keys.map("n", "<leader>b",    "<CMD>Pick git_branches<CR>", "Pick git branches")
+    -- keys.map("n", "<leader>b",    "<CMD>Pick git_commits<CR>", "Pick git commits")
+    -- keys.map("n", "<leader>b",    "<CMD>Pick git_files<CR>", "Pick git files")
+    -- keys.map("n", "<leader>b",    "<CMD>Pick git_hunks<CR>", "Pick git hunks")
+    -- end
+
+    ---@diagnostic disable-next-line: undefined-global
+    vim.ui.select = MiniPick.ui_select
+  end
+
+  local mini_notify = require "mini.notify"
+  if mini_notify then
+    mini_notify.setup {
+      content = {
+        format = function(notif)
+          return notif.msg
+        end,
+      },
+      lsp_progress = {
+        enable = true,
+        duration_last = 1000,
+      },
+      window = {
+        config = {
+          border = 'none',
+          anchor = 'SE',
+          col = vim.api.nvim_win_get_width(0) -2, -- because of the scrollbar
+          row = vim.api.nvim_win_get_height(0),
+        },
+        winblend = 0,
+      },
+    }
+
+    -- vim.notify = mini_notify.make_notify()
+  end
+
+  local mini_diff = require 'mini.diff'
+  if mini_diff then
+    mini_diff.setup {
+      view = {
+        style = 'sign',
+        signs = { add = '│', change = '│', delete = '-' },
+      },
+      mappings = {
+        apply = 'gs',
+        reset = 'gr',
+        textobject = 'gh',
+        goto_prev = 'g<Left>',
+        goto_next = 'g<Right>',
+        -- goto_first = '[H',
+        -- goto_last = ']H',
+      },
+    }
+  end
+
+  local mini_git = require 'mini.git'
+  if mini_git then
+    mini_git.setup {}
+  end
+
+  -- TODO: clue
+  -- local mini_clue = require 'mini.clue'
+  -- if mini_clue then
+  --   mini_clue.setup {}
+  -- end
+
+  -- TODO: surround
+  -- local mini_surround = require 'mini.surround'
+  -- if mini_surround then
+  --   mini_surround.setup()
+  -- end
+
+  -- TODO: operators
+  -- require 'mini.operators'.setup()
+
+  -- TODO: align
+  -- require 'mini.align'.setup()
+
+  -- TODO: pairs
+  -- require 'mini.pairs'.setup()
+
   -- local mini_files = require 'mini.files'
   -- if mini_files then
   --   mini_files.setup {
@@ -234,117 +359,6 @@ M.config = function()
   --     end
   --   })
   -- end
-
-  local mini_trailspace = require 'mini.trailspace'
-  if mini_trailspace then
-    mini_trailspace.setup()
-    vim.api.nvim_create_user_command('Trim', mini_trailspace.trim, {})
-    vim.api.nvim_create_user_command('TrimLastLines', mini_trailspace.trim_last_lines, {})
-    vim.api.nvim_create_user_command('TrimAll', function()
-      mini_trailspace.trim()
-      mini_trailspace.trim_last_lines()
-    end, {})
-  end
-
-  local mini_extra = require "mini.extra"
-
-  local mini_pick = require 'mini.pick'
-  if mini_pick then
-    mini_pick.setup {
-      mappings = {
-        -- choose_marked = "<C-q>",
-      },
-      window = {
-        config = function()
-          height = math.floor(0.6 * vim.o.lines)
-          width = math.floor(0.6 * vim.o.columns)
-
-          return {
-            anchor = 'NW',
-            height = height,
-            width = width,
-            row = math.floor(0.5 * (vim.o.lines - height)),
-            col = math.floor(0.5 * (vim.o.columns - width)),
-          }
-        end
-      },
-    }
-
-    keys.map("n", "<leader><CR>", "<CMD>Pick resume<CR>", "Pick resume")
-    keys.map("n", "<leader>f",    "<CMD>Pick files<CR>", "Pick find files")
-    keys.map("n", "<leader>g",    "<CMD>Pick grep_live<CR>", "Pick grep string")
-    keys.map("n", "<leader>h",    "<CMD>Pick help<CR>", "Pick help pages")
-
-    -- if mini_extra then
-      -- keys.map("n", "<leader>b",    "<CMD>Pick buffer_lines<CR>", "Pick buffer lines")
-      -- keys.map("n", "<leader>b",    "<CMD>Pick diagnostic<CR>", "Pick lsp diagnostics")
-      -- keys.map("n", "<leader>b",    "<CMD>Pick explorer<CR>", "Pick file tree explorer")
-      -- keys.map("n", "<leader>b",    "<CMD>Pick git_branches<CR>", "Pick git branches")
-      -- keys.map("n", "<leader>b",    "<CMD>Pick git_commits<CR>", "Pick git commits")
-      -- keys.map("n", "<leader>b",    "<CMD>Pick git_files<CR>", "Pick git files")
-      -- keys.map("n", "<leader>b",    "<CMD>Pick git_hunks<CR>", "Pick git hunks")
-    -- end
-
-    ---@diagnostic disable-next-line: undefined-global
-    vim.ui.select = MiniPick.ui_select
-  end
-
-  local mini_notify = require "mini.notify"
-  if mini_notify then
-    mini_notify.setup {
-      content = {
-        format = function(notif)
-          return notif.msg
-        end,
-      },
-      lsp_progress = {
-        enable = true,
-        duration_last = 1000,
-      },
-      window = {
-        config = {
-          border = 'none',
-          anchor = 'SE',
-          col = vim.api.nvim_win_get_width(0) -2, -- because of the scrollbar
-          row = vim.api.nvim_win_get_height(0),
-        },
-        winblend = 0,
-      },
-    }
-
-    -- vim.notify = mini_notify.make_notify()
-  end
-
-  local mini_diff = require 'mini.diff'
-  if mini_diff then
-    mini_diff.setup {
-      view = {
-        style = 'sign',
-        signs = { add = '│', change = '│', delete = '-' },
-      }
-    }
-  end
-
-  -- TODO: clue
-  -- local mini_clue = require 'mini.clue'
-  -- if mini_clue then
-  --   mini_clue.setup {}
-  -- end
-
-  -- TODO: surround
-  -- local mini_surround = require 'mini.surround'
-  -- if mini_surround then
-  --   mini_surround.setup()
-  -- end
-
-  -- TODO: operators
-  -- require 'mini.operators'.setup()
-
-  -- TODO: align
-  -- require 'mini.align'.setup()
-
-  -- TODO: pairs
-  -- require 'mini.pairs'.setup()
 end
 
 return M

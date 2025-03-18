932b8a5663645d970355b9a8206410a5d5985478
Raafat Turki <raafat.turki@proton.me>
2025-03-18 20:53:34 +0300

    feat: add snack profiler

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
+if vim.env.PROF then
+  local snacks = vim.fn.stdpath("data") .. "/lazy/snacks.nvim"
+  vim.opt.rtp:append(snacks)
+  require("snacks.profiler").startup({
+    startup = {
+      -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+      event = "UIEnter",
+      -- event = "VeryLazy",
+    },
+  })
+end
+
 log = require "helpers.logger"
 
 function prequire(name)

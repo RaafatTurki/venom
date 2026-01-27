e048f70fa6fe65d7ee04c3eabd3941c37c64653d
Raafat Turki <raafat.turki@proton.me>
2026-01-27 16:19:00 +0300

    feat: add xdg-open to neotree

--- lua/plugins/05_neotree.lua
+++ lua/plugins/05_neotree.lua
@@ -14,6 +14,12 @@ require "neo-tree".setup {
   use_popups_for_input = false,
 
   filesystem = {
+    window = {
+      mappings = {
+        ["o"] = "system_open",
+      },
+    },
+
     filtered_items = {
       hide_dotfiles = false,
       hide_gitignored = false,
@@ -27,6 +33,27 @@ require "neo-tree".setup {
         -- ".DS_Store",
       },
     },
+  },
+
+  commands = {
+    system_open = function(state)
+      local node = state.tree:get_node()
+      local path = node:get_id()
+      -- macOs: open file in default application in the background.
+      -- vim.fn.jobstart({ "open", path }, { detach = true })
+      -- Linux: open file in default application
+      vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+
+      -- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+      local p
+      local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+      if lastSlashIndex then
+        p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
+      else
+        p = path -- If no slash found, return original path
+      end
+      vim.cmd("silent !start explorer " .. p)
+    end,
   },
 
   event_handlers = {

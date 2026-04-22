328ace3b91bd2d272fc27591c5fd19731aaf2df1
Raafat Turki <raafat.turki@proton.me>
2026-04-22 05:58:20 +0300

    chore: patch treesitter predicates

--- lua/plugins/06_treesitter.lua
+++ lua/plugins/06_treesitter.lua
@@ -1,4 +1,151 @@
 local ts = require "nvim-treesitter"
+local query = require "vim.treesitter.query"
+
+-- TODO: remove once unneeded
+local function patch_query_predicates_for_nvim_012()
+  if vim.fn.has("nvim-0.12") ~= 1 then
+    return
+  end
+
+  local html_script_type_languages = {
+    ["importmap"] = "json",
+    ["module"] = "javascript",
+    ["application/ecmascript"] = "javascript",
+    ["text/ecmascript"] = "javascript",
+  }
+
+  local non_filetype_match_injection_language_aliases = {
+    ex = "elixir",
+    pl = "perl",
+    sh = "bash",
+    uxn = "uxntal",
+    ts = "typescript",
+  }
+
+  local function get_parser_from_markdown_info_string(injection_alias)
+    local match = vim.filetype.match({ filename = "a." .. injection_alias })
+    return match or non_filetype_match_injection_language_aliases[injection_alias] or injection_alias
+  end
+
+  local function valid_args(name, pred, count, strict_count)
+    local arg_count = #pred - 1
+
+    if strict_count then
+      if arg_count ~= count then
+        vim.api.nvim_err_writeln(string.format("%s must have exactly %d arguments", name, count))
+        return false
+      end
+    elseif arg_count < count then
+      vim.api.nvim_err_writeln(string.format("%s must have at least %d arguments", name, count))
+      return false
+    end
+
+    return true
+  end
+
+  local function capture_node(match, id)
+    local nodes = match[id]
+    if not nodes then
+      return nil
+    end
+    if type(nodes) ~= "table" then
+      return nodes
+    end
+    return nodes[1]
+  end
+
+  local opts = { force = true, all = false }
+
+  query.add_predicate("nth?", function(match, _pattern, _bufnr, pred)
+    if not valid_args("nth?", pred, 2, true) then
+      return
+    end
+
+    local node = capture_node(match, pred[2])
+    local n = tonumber(pred[3])
+    if node and node:parent() and node:parent():named_child_count() > n then
+      return node:parent():named_child(n) == node
+    end
+
+    return false
+  end, opts)
+
+  query.add_predicate("is?", function(match, _pattern, bufnr, pred)
+    if not valid_args("is?", pred, 2) then
+      return
+    end
+
+    local locals = require "nvim-treesitter.locals"
+    local node = capture_node(match, pred[2])
+    local types = { unpack(pred, 3) }
+
+    if not node then
+      return true
+    end
+
+    local _, _, kind = locals.find_definition(node, bufnr)
+    return vim.tbl_contains(types, kind)
+  end, opts)
+
+  query.add_predicate("kind-eq?", function(match, _pattern, _bufnr, pred)
+    if not valid_args(pred[1], pred, 2) then
+      return
+    end
+
+    local node = capture_node(match, pred[2])
+    local types = { unpack(pred, 3) }
+
+    if not node then
+      return true
+    end
+
+    return vim.tbl_contains(types, node:type())
+  end, opts)
+
+  query.add_directive("set-lang-from-mimetype!", function(match, _, bufnr, pred, metadata)
+    local capture_id = pred[2]
+    local node = capture_node(match, capture_id)
+    if not node then
+      return
+    end
+
+    local type_attr_value = vim.treesitter.get_node_text(node, bufnr)
+    local configured = html_script_type_languages[type_attr_value]
+    if configured then
+      metadata["injection.language"] = configured
+    else
+      local parts = vim.split(type_attr_value, "/", {})
+      metadata["injection.language"] = parts[#parts]
+    end
+  end, opts)
+
+  query.add_directive("set-lang-from-info-string!", function(match, _, bufnr, pred, metadata)
+    local capture_id = pred[2]
+    local node = capture_node(match, capture_id)
+    if not node then
+      return
+    end
+
+    local injection_alias = vim.treesitter.get_node_text(node, bufnr):lower()
+    metadata["injection.language"] = get_parser_from_markdown_info_string(injection_alias)
+  end, opts)
+
+  query.add_directive("downcase!", function(match, _, bufnr, pred, metadata)
+    local id = pred[2]
+    local node = capture_node(match, id)
+    if not node then
+      return
+    end
+
+    local text = vim.treesitter.get_node_text(node, bufnr, { metadata = metadata[id] }) or ""
+    if not metadata[id] then
+      metadata[id] = {}
+    end
+    metadata[id].text = string.lower(text)
+  end, opts)
+end
+
+patch_query_predicates_for_nvim_012()
 
 vim.api.nvim_create_autocmd("PackChanged", {
   group = vim.api.nvim_create_augroup("pack-build-treesitter", { clear = true }),

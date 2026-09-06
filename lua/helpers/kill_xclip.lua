868d945d4b274fd0abafa2bf2ddc919ba0c95dcc
Raafat Turki <raafat.turki@proton.me>
2026-09-06 05:16:55 +0300

    chore: remove kill_xclip

--- lua/helpers/kill_xclip.lua
+++
@@ -1,7 +0,0 @@
--- kill xclip on VimLeave if last nvim instance is closed
-vim.api.nvim_create_autocmd("VimLeave", {
-  group = vim.api.nvim_create_augroup("KillXclip", { clear = true }),
-  callback =  function()
-    os.execute("pkill xclip")
-  end
-})
